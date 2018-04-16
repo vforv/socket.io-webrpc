6dc3e4dd8af6a0743eb68c88d08f151cf17bf7d5
Vladimir Djukic <vladimir@amerbank.com>
2018-04-16 17:30:23 +0200

    add h

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = new express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors')
 
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
     res.redirect('index.html');
 });
+
+io.origins('*:*');
 
 io.on('connection', function (socket) {
     socket.on('stream', function (image) {
